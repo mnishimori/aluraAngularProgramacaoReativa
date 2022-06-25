341891a41aaaa1b3460292394678abd2543202fe
mnishimori <mnishimori@yahoo.com.br>
2022-06-25 11:14:38 -0300

    Observables, debugar um fluxo de um observable, tap, map, pipe async

--- src/app/acoes/acoes.service.ts
+++ src/app/acoes/acoes.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
+import {map, pluck, tap} from 'rxjs/operators';
+import {Acao, Acoes, AcoesApi} from './modelo/acoes';
+import {Observable} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
@@ -8,7 +11,24 @@ export class AcoesService {
 
   constructor(private http: HttpClient) { }
 
-  getAcoes(){
-    return this.http.get<any>(`http://localhost:3000/acoes`);
+  getAcoes(): Observable<Acoes>{
+    return this.http.get<AcoesApi>(`http://localhost:3000/acoes`)
+      .pipe(
+        tap((valor) => console.log(valor)),
+        // map((api) => api.payload), o pluck faz a mesma coisa que este map
+        pluck('payload'),
+        tap( (valor) => console.log(valor)),
+        map((acoes) => acoes.sort((acaoA, acaoB) => this.ordenaPorCodigo(acaoA, acaoB)))
+      );
+  }
+
+  private ordenaPorCodigo(acaoA: Acao, acaoB: Acao) {
+    if (acaoA.codigo > acaoB.codigo) {
+      return 1;
+    }
+    if (acaoA.codigo < acaoB.codigo) {
+      return -1;
+    }
+    return 0;
   }
 }
